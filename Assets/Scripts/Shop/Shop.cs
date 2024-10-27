c4ee0d3a5991079c009686948b83dda91684ce51
Bastien SANTE <b.sante@nouvellesimages.xyz>
2024-10-27 16:03:38 +0100

    Very minor changes

--- Assets/Scripts/Shop/Shop.cs
+++ Assets/Scripts/Shop/Shop.cs
@@ -1,11 +1,9 @@
-using System;
 using System.Collections.Generic;
-using Entity;
 using Player;
 using UnityEngine;
 
-
 [System.Serializable]
+// ReSharper disable once CheckNamespace
 public class Shop : MonoBehaviour
 {
     public List<ShopItem> shopList;
@@ -14,15 +12,20 @@ public class Shop : MonoBehaviour
     public bool IsEmpty => shopList.Count == 0;   //Is the inventory empty ?
     public bool IsFull => shopList.Count >= 5;    //Are there more than 5 items ?
 
-    public void Purchase(ShopItem shopItem)
+    public int Purchase(ShopItem shopItem)
     {
+        Debug.Log("fuck");
+        
         if (depositInventory.currencyAmount < shopItem.price)
         {
             Debug.Log("Insufficient funds!");
-            return;
+            return 0;
         }
         
+        Debug.Log($"Purchased {shopItem}");
         depositInventory.itemList.Add(new Item(shopItem.SO_ShpSlot, 1));
+
+        return 0;
     }
 }
 
